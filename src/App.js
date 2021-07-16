0e8f1083b6b4d3a3b11193bed4952136f9803e16
Shakik Ahmed Chowdhury <sakikiut@yahoo.com>
2021-07-16 11:10:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/cart'
+import Default from './components/Default'
+import Modal from './components/Modal';
+
+
+function App() {
+  return (
+   <React.Fragment>
+
+    <Navbar></Navbar>
+<Switch>
+  <Route exact path='/' component={ProductList}></Route>
+  <Route path='/details' component={Details}></Route>
+  <Route path='/cart' component={Cart}></Route>
+  <Route component={Default}></Route>
+</Switch>
+
+    <Modal></Modal>
+    
+
+   </React.Fragment>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/Details.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { ProductConsumer } from '../context'
+import { Link } from 'react-router-dom'
+import {ButtonContaner} from './Button'
+
+export default class Details extends Component {
+    render() {
+        return (
+            <ProductConsumer>
+                {(value)=>{
+
+                    const {id, company,img, info, price, title, inCart} = value.detailProduct
+
+                    return (
+
+                        <div className="container py-5" >
+
+                            <div className='row'>
+
+                                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+
+                                    <h1>{title}</h1>
+                                </div>
+                            </div>
+
+                           <div className='row'>
+                               <div className='col-10 mx-auto col-md-6 my-3'>
+
+                                <img src={img} className='image-fluid' alt='product' />
+                               </div>
+
+                               <div className='col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                                <h2>model: {title}</h2>
+                                   <h4 className='text-title text-uppercase text-muted mt-3 mb-2'>made by: <span className='text-uppercase'>{company}</span></h4>
+                                   <h4 className='text-blue'>
+                                       <strong>price: <span>$</span>{price}</strong>
+                                       <p className='text-capitalize font-weight-bold mt-3 mb-0'>Info:</p>
+                                       <p className='text-muted lead'>{info}</p>
+                                   </h4>
+                                   <div>
+
+                                       <Link to='/'>
+                                           <ButtonContaner>back to products</ButtonContaner>
+                                       </Link>
+
+                                       <ButtonContaner cart disabled={inCart?true:false} onClick={()=>{
+
+                                        value.addToCart(id)
+                                        value.openModal(id)
+
+                                       }}>
+                                           {inCart? 'inCart': 'add to cart'}
+                                       </ButtonContaner>
+                                   </div>
+                               </div>
+                           </div>
+                        </div>
+
+                    )
+                }}
+            </ProductConsumer>
+        )
+    }
+}
+
